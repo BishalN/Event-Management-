fc2db9564b6a011309da336553a1502c8f9067a8
Bishal Neupane <neupanebishal07@gmail.com>
2020-12-27 16:45:59 +0545

    Frontend Auth

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use(
@@ -29,7 +39,7 @@ mongoose
   )
   .then(() => {
     console.log('App is running');
-    app.listen(3000);
+    app.listen(8000);
   })
   .catch((error) => {
     console.log(error.message);
--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+
+import FormContainer from '../components/FormContainer';
 
 const AuthPage = () => {
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLogin, setIsLogin] = useState(true);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    let requestBody = {
+      query: `
+        query {
+          login(email:"${emailAddress}",password:"${password}"){
+            userId,
+            token,
+            tokenExpiration
+          }
+        }
+      `,
+    };
+
+    if (!isLogin) {
+      requestBody = {
+        query: ` mutation {createUser(userInput:{email:"${emailAddress}",password:"${password}"}){_id email
+          }
+        }
+        `,
+      };
+    }
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
-    <div>Hello this is the authentication page of my event management app</div>
+    <FormContainer>
+      <Form onSubmit={submitHandler} className='mt-5'>
+        <Form.Group controlId='formBasicEmail'>
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={emailAddress}
+            onChange={(e) => setEmailAddress(e.target.value)}
+          />
+          <Form.Text className='text-muted'>
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
+        <Button className='ml-4' onClick={() => setIsLogin(!isLogin)}>
+          Switch To {isLogin ? 'Sign Up' : 'Login'}
+        </Button>
+        <span className='ml-4'>
+          You are in {isLogin ? 'Login mode' : 'Sign up Mode'}
+        </span>
+      </Form>
+    </FormContainer>
   );
 };
 
